6203ce0bafdcdfd493d8b520a28fb7e0c8ec5e73
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2023-03-20 10:03:06 +0300

    Updated getAllPurchaseOfUser method

--- src/main/java/com/example/springbootbooksellerapp/controller/PurchaseHistoryController.java
+++ src/main/java/com/example/springbootbooksellerapp/controller/PurchaseHistoryController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/purchase-history")
@@ -27,6 +24,8 @@ public class PurchaseHistoryController {
         return  new ResponseEntity<>(purchaseHistoryService.savePurchaseHistory(purchaseHistory), HttpStatus.CREATED);
     }
 
+
+    @GetMapping//api/purchase-history
     public  ResponseEntity<?> getAllPurchaseOfUser(@AuthenticationPrincipal UserPrincipal userPrincipal){
 
         return ResponseEntity.ok(purchaseHistoryService.findAllPurchasesOfUser(userPrincipal.getId()));
